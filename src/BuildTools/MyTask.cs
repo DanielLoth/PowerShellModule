a8a105ff639c603c935c4cab06e38806a438056e
Daniel Loth <daniel.j.loth@gmail.com>
2021-07-25 18:04:02 +1000

    Experiment with MSBuild Task extensibility

---
+++ src/BuildTools/MyTask.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace BuildTools
+{
+    public sealed class MyTask : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "MyTask message");
+            return true;
+        }
+    }
+}
